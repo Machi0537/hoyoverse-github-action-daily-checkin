3701b714dcccb3fdcd592237e4c20d92b4a8254b
Machi0537 <139733245+Machi0537@users.noreply.github.com>
2023-12-27 20:14:12 +0800

    Update CheckInDailyTest.java

--- src/test/java/com/zvyap/dailycheckin/test/CheckInDailyTest.java
+++ src/test/java/com/zvyap/dailycheckin/test/CheckInDailyTest.java
@@ -34,7 +34,7 @@ public class CheckInDailyTest {
                         .game(GameType.HONKAI_STAR_RAIL)
                         .build()
 
-              CheckInAction.builder()
+               CheckInAction.builder()
                         .token(HoyoToken.of(System.getenv("USER_3_ID"), System.getenv("USER_3_TOKEN")))
                         .game(GameType.GENSHIN_IMPACT)
                         .game(GameType.HONKAI_STAR_RAIL)
